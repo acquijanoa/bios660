0a80cd986c4cc7475d5d1448c2984e7c44331a38
acquijanoa <acquijanoa@unal.edu.co>
2021-09-12 15:58:54 -0400

    R Code

--- HW3/hw3.R
+++ HW3/hw3.R
@@ -19,6 +19,7 @@ df %>%  head() ### Displaying first 5 records in the dataset
 ### Extracting the information about mean, sd and count by CHD_event variable
 df %>%  group_by(CHD_Event) %>% summarise(mu = mean(Cholesterol), 
                                           sd = sd(Cholesterol), 
+                                          var = var(Cholesterol),
                                           n = n())
 
 ### Result
@@ -26,8 +27,8 @@ df %>%  group_by(CHD_Event) %>% summarise(mu = mean(Cholesterol),
 # CHD_Event    mu    sd     n
 #   1         0  200.  35.9   460
 #   2         1  213.  33.3    40
-
-Sp2 = (39*(33.3^2) + 459*(35.9^2) ) / 498
+sqrt(1107)
+Sp2 = ((39*(33.3^2)) + (459*(35.9^2)) ) / 498
 Sp2
 Sp = sqrt(Sp2); Sp
 # [1] 35.70322
@@ -35,12 +36,14 @@ Sp = sqrt(Sp2); Sp
 tc = 13  / ( Sp * sqrt( (1/40) + (1/460)  ) ); tc
 #[1] 2.208818
 
-t.test( df %>% 
-          filter(CHD_Event == 1)  %>% 
-          select(Cholesterol), 
-        df %>% 
-          filter(CHD_Event == 0)  %>% 
-          select(Cholesterol) )
+### Validating results with t.test function
+x = df %>% 
+  filter(CHD_Event == 1)  %>% 
+  select(Cholesterol)
+y = df %>% 
+  filter(CHD_Event == 0)  %>% 
+  select(Cholesterol)
+t.test(x, y , var.equal = TRUE )
 
 
 
