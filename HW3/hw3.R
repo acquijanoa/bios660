22c7077787b3c1eb2d8ce24d4109418cf5e1265a
acquijanoa <acquijanoa@unal.edu.co>
2021-09-12 15:11:29 -0400

    R Code

--- HW3/hw3.R
+++ HW3/hw3.R
@@ -12,7 +12,7 @@ library(ggplot2)
 library(dplyr)
 
 ## Reading data from folder
-df = read.table('data/HW3_CHOL.txt')
+df = read.table('HW3/data/HW3_CHOL.txt')
 names(df) = c('Gender', 'Age','Cholesterol', 'CHD_Event') ## Assigning column names
 df %>%  head() ### Displaying first 5 records in the dataset
 
@@ -20,3 +20,10 @@ df %>%  head() ### Displaying first 5 records in the dataset
 df %>%  group_by(CHD_Event) %>% summarise(mu = mean(Cholesterol), 
                                           sd = sd(Cholesterol), 
                                           n = n())
+
+### Result
+# A tibble: 2 x 4
+# CHD_Event    mu    sd     n
+#   1         0  200.  35.9   460
+#   2         1  213.  33.3    40
+
