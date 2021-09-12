6c7f49e4deb9b396b141a539042df287c2606ed8
acquijanoa <acquijanoa@unal.edu.co>
2021-09-12 17:54:25 -0400

    Boostratp confidence interval

--- HW3/hw3.R
+++ HW3/hw3.R
@@ -17,25 +17,27 @@ names(df) = c('Gender', 'Age','Cholesterol', 'CHD_Event') ## Assigning column na
 df %>%  head() ### Displaying first 5 records in the dataset
 
 ### Extracting the information about mean, sd and count by CHD_event variable
-df %>%  group_by(CHD_Event) %>% summarise(mu = mean(Cholesterol), 
+options(pillar.sigfig=5)
+df %>%  group_by(CHD_Event) %>% summarise(mu = round(mean(Cholesterol),2 ), 
                                           sd = sd(Cholesterol), 
                                           var = var(Cholesterol),
                                           n = n())
-
 ### Result
 # A tibble: 2 x 4
-# CHD_Event    mu    sd     n
-#   1         0  200.  35.9   460
-#   2         1  213.  33.3    40
+# CHD_Event    mu    sd   var     n
+# <int> <dbl> <dbl> <dbl> <int>
+#   1         0 199.85 35.87 1287.   460
+#   2         1 213.02 33.28 1107.    40
 sqrt(1107)
 Sp2 = ((39*(33.3^2)) + (459*(35.9^2)) ) / 498
 Sp2
 Sp = sqrt(Sp2); Sp
 # [1] 35.70322
 
-tc = 13  / ( Sp * sqrt( (1/40) + (1/460)  ) ); tc
-#[1] 2.208818
+tc = (213.02 - 199.85) / ( Sp * sqrt( (1/40) + (1/460)  ) ); tc
+# [1] 2.237702
 
+2* (1 - pt(2.2404, 498))
 ### Validating results with t.test function
 x = df %>% 
   filter(CHD_Event == 1)  %>% 
@@ -43,7 +45,48 @@ x = df %>%
 y = df %>% 
   filter(CHD_Event == 0)  %>% 
   select(Cholesterol)
+
 t.test(x, y , var.equal = TRUE )
 
+## T-student quantiles
+
+qt(0.975, 498) # 1.964739
+qt(0.025, 498) # -1.964739
 
 
+
+#### b) Variances test 
+
+Fc = var(y$Cholesterol)/  var(x$Cholesterol); Fc
+# [1] 1.162018
+
+Ft = qf(0.975, df1 = 459, 39); Ft
+# [1] 1.672317
+
+Ft2 = qf(0.025, df1 = 459, 39); Ft2
+# [1] 0.6559809
+
+### c) Bootstrap method
+
+db = df %>%  
+  filter(Gender == 'M' & 
+           CHD_Event == 1 ) %>% 
+  .$Cholesterol
+nb = length(db); nb
+meanb = mean(db); meanb
+Zb = vector()
+
+## Boostrap method
+for(i in 1:10000){
+  sampleb = sample(db, 
+                   size = nb, 
+                   replace = T)
+  Zb[i] = (mean(sampleb) - meanb ) / sqrt( var(sampleb) / nb )
+}
+
+plot(density(Zb), 
+     main = "Boostrap sampling distribution for Z*(B) values")
+
+
+quantile(Zb, 0.975)
+quantile(Zb, 0.025)
